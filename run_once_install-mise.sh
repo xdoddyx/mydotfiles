709c24171c43b3b132e6b9941aad57b9f3a9bde8
Doddy Setyawan <xdoddyx@yahoo.com>
2025-07-15 03:35:22 +0000

    Update

--- run_once_install-mise.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Exit if any command fails
-set -e
-
-echo "--- [Chezmoi] Installing mise ---"
-# The official installer, which places the binary in ~/.local/bin/mise
-curl https://mise.run | sh
